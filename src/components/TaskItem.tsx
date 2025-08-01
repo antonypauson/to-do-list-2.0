f9ba33d8e389b9474976bfe49ef581699a956d82
antonypauson <13antonysun@gmail.com>
2025-08-01 19:25:22 +0530

    refactor: fixing alignment and non-important changes

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,10 +1,10 @@
-export default function TaskItem({task}) {
+export default function TaskItem({task, onDelete, onEdit}) {
     return (
         <li className="task-li flex items-center gap-4">
             <p className="flex-grow">{task.text}</p>
             <div className="icon-container flex flex-shrink-0 gap-4 p-0">
-                <img className="w-[45px] hover:scale-120 transition cursor-pointer" src="/src/assets/edit-button.svg"/>
-                <img className="w-[45px] hover:scale-120 transition cursor-pointer" src="/src/assets/remove-button.svg"/>
+                <img className="w-[45px] hover:scale-120 transition cursor-pointer" src="/src/assets/edit-button.svg" onClick={onEdit}/>
+                <img className="w-[45px] hover:scale-120 transition cursor-pointer" src="/src/assets/remove-button.svg" onClick={onDelete}/>
             </div>
         </li>
     )
