82f8966c88df64ebfe043264a1c74c6ce3aac970
antonypauson <13antonysun@gmail.com>
2025-08-11 01:39:59 +0530

    feat(deleteall): enabled deleteAll functionality

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,10 +1,10 @@
 import { Stack } from "@mui/material"
 import {Button} from "@mui/material"
 
-export default function FooterButtons() {
+export default function DeleteButton({onDeleteAll}) {
   return (
     <Stack direction="row" spacing={2}>
-      <Button variant="contained">Delete All</Button>
+      <Button variant="contained" onClick={onDeleteAll}>Delete All</Button>
     </Stack>
   )
 }
