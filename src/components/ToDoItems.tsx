1e301d1c813035ba83a134f335e954dc58e0e7f9
antonypauson <13antonysun@gmail.com>
2025-08-10 21:35:53 +0530

    feat(todoitems): added Stack for Items, Icons from MUI

--- src/components/ToDoItems.tsx
+++ src/components/ToDoItems.tsx
@@ -1,3 +1,17 @@
-export default function ToDoItems() {
-  return <h1>I'm At TodoItems</h1>;
+import  Stack  from "@mui/material/Stack"; 
+import Box from "@mui/material/Box"; 
+import Item from "./Item";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+export default function ToDoItems({tasks}) {
+  return (
+    <Box sx={{width: '80%', margin: 'auto'}}>
+      <Stack direction="column" spacing={2}>
+        {tasks.map(eachTask => 
+          <Item>{eachTask.name} <EditIcon/> <DeleteIcon/></Item>
+        )}
+      </Stack>
+    </Box>
+  )
 }
