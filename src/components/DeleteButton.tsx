0403849c885b8618c1c6aa6792893d46bf1c1bd9
antonypauson <13antonysun@gmail.com>
2025-08-11 01:49:05 +0530

    feat(deletebutton): remove deletebutton if emtpy

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "@mui/material"
 import {Button} from "@mui/material"
 
-export default function DeleteButton({onDeleteAll}) {
+export default function DeleteButton({onDeleteAll, empty}) {
   return (
     <Stack direction="row" spacing={2}>
-      <Button variant="contained" onClick={onDeleteAll}>Delete All</Button>
+      {!empty &&
+        (<Button variant="contained" onClick={onDeleteAll}>
+          Delete All
+        </Button>)
+      }
     </Stack>
-  )
+  );
 }
